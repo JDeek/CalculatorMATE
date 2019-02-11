6c2bbe6c5e5861f7a51960d476125b6e1db81c00
JDEEK <jumper-2017@mail.ru>
2019-02-11 18:58:24 +0200

    project remove

--- MATECalculator/src/com/mate/academy/operations/Operations.java
+++
@@ -1,9 +0,0 @@
-package com.mate.academy.operations;
-
-public interface Operations {
-
-    double add(String a, String b);
-    double subtract(String a, String b);
-    double divide(String a, String b);
-    double multiple(String a, String b);
-}
